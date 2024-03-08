71b57dddf5a3ee50e2df3b0a48f32f5dfa129e15
Shahrukh <shahrukhali10713@gmail.com>
2024-03-08 23:51:01 +0530

    create github repo

---
+++ src/main/java/com/example/demo/SpringBootNotesApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNotesApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello Srk: How r u");
+
+		SpringApplication.run(SpringBootNotesApplication.class, args);
+	}
+
+}
